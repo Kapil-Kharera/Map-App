655a5d7dd5a16a37b0ab7d9e028598ff1050b6f5
KapilKharera <kapil95881@gmail.com>
2022-12-14 00:21:02 +0530

    Add method to display Map Marker

--- script.js
+++ script.js
@@ -9,15 +9,35 @@ if (navigator.geolocation) {
         console.log(latitude, longitude, timestamp);
         console.log(`https://www.google.com.pt/maps/@${latitude},${longitude}`);
 
-        var map = L.map('map').setView([29.17162171686164, 75.73536437989269], 13);
+        const coords = [latitude, longitude]
+
+        const map = L.map('map').setView(coords, 13);
+        console.log(map);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([29.17162171686164, 75.73536437989269]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+       
+
+        map.on('click', function(mapEvent) {
+            console.log(mapEvent);
+            
+            const {lat, lng} = mapEvent.latlng;
+            console.log(lat, lng);
+
+            L.marker([lat, lng]).addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
             .openPopup();
+
+        });
     }, function () {
         alert('Could not get your position');
     });
